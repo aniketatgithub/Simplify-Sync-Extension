4b9745fb1012f126f5b6eb029cd795fc400b1b33
Aniket <aniketkomailkro@gmail.com>
2024-01-01 16:11:44 -0800

    Redundant code removal

--- background.js
+++ background.js
@@ -8,11 +8,6 @@ function isSimplifyInternshipPage(url) {
   return url.startsWith(simplifyInternshipUrl);
 }
 
-chrome.idle.onStateChanged.addListener((browserActivityState) => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const activeTab = tabs[0];
-    if (activeTab && activeTab.url && activeTab.url.match('https:\/\/github\.com\/SimplifyJobs\/Summer2024-Internships.*')) {
-      chrome.tabs.sendMessage(activeTab.id, { browserActivityState: browserActivityState });
-    }
-  });
-});
+
+
+
